1813ecc9c48b6b43b263b8f1f33a399bea0d25d7
Corey <corey.r.girard@gmail.com>
2018-04-01 17:06:08 -0400

    Tests and linting

--- scrape.py
+++ scrape.py
@@ -1,14 +1,14 @@
 import re
 
-def makeUrl(url):
+def make_url(url):
     '''
-    >>> makeUrl('Logic')
+    >>> make_url('Logic')
     'http://en.wikipedia.org/wiki/Logic'
     '''
 
     return 'http://en.wikipedia.org/wiki/'+url
 
-def getBody(page):
+def get_body(page):
     match = re.search(r'From Wikipedia, the free encyclopedia',page)
     start = match.end()
     end = len(page)
@@ -26,7 +26,7 @@ def getBody(page):
     return page[start:end]
 
 # get only text/HTML within <p></p> tags
-def getParagraphs(page):
+def get_paragraphs(page):
     temp = []
     for m in re.finditer(r'<p>(.*?)</p>',page):
         if m:
@@ -34,25 +34,25 @@ def getParagraphs(page):
             temp.append(' ')
     return (' '.join(temp)).strip()
 
-def stripTags(page):
+def strip_tags(page):
     '''
-    >>> stripTags('counterfactuals, such as <i>If the moon is made of green cheese, then 2+2=5</i>, which are puzzling because natural language does not support the <a href="/wiki/Principle_of_explosion" title="Principle of explosion">principle of explosion</a>.')
+    >>> strip_tags('counterfactuals, such as <i>If the moon is made of green cheese, then 2+2=5</i>, which are puzzling because natural language does not support the <a href="/wiki/Principle_of_explosion" title="Principle of explosion">principle of explosion</a>.')
     'counterfactuals, such as If the moon is made of green cheese, then 2+2=5, which are puzzling because natural language does not support the principle of explosion.'
     '''
 
     return re.sub(r'<.*?>',r'',page)
 
-def stripBrackets(page):
+def strip_brackets(page):
     '''
-    >>> stripBrackets('cannot be consistent and complete;[4] however,')
+    >>> strip_brackets('cannot be consistent and complete;[4] however,')
     'cannot be consistent and complete; however,'
     '''
 
     return re.sub(r'\[.*?\]',r'',page)
 
-def stripCharacters(page):
+def strip_characters(page):
     '''
-    >>> stripCharacters('by means of "if&#160;... then&#160;...", due to')
+    >>> strip_characters('by means of "if&#160;... then&#160;...", due to')
     'by means of "if ;... then ;...", due to'
     '''
 
@@ -61,66 +61,66 @@ def stripCharacters(page):
     page = re.sub(r'[ ]+',r' ',page)
     return page
 
-def cleanText(text):
+def clean_text(text):
     '''
-    >>> cleanText('controversy in <a href="/wiki/Metaphysics" title="Metaphysics">metaphysics</a> on <a href="/wiki/Realism_versus_anti-realism" class="mw-redirect" title="Realism versus anti-realism">realism versus anti-realism</a>.</p> <h3><span class="mw-headline" id="Implication:_Strict_or_material">Implication: Strict or material</span><span class="mw-editsection"><span class="mw-editsection-bracket">[</span><a href="/w/index.php?title=Logic&amp;action=edit&amp;section=21" title="Edit section: Implication: Strict or material">edit</a><span class="mw-editsection-bracket">]</span></span></h3><div role="note" class="hatnote navigation-not-searchable">')
+    >>> clean_text('controversy in <a href="/wiki/Metaphysics" title="Metaphysics">metaphysics</a> on <a href="/wiki/Realism_versus_anti-realism" class="mw-redirect" title="Realism versus anti-realism">realism versus anti-realism</a>.</p> <h3><span class="mw-headline" id="Implication:_Strict_or_material">Implication: Strict or material</span><span class="mw-editsection"><span class="mw-editsection-bracket">[</span><a href="/w/index.php?title=Logic&amp;action=edit&amp;section=21" title="Edit section: Implication: Strict or material">edit</a><span class="mw-editsection-bracket">]</span></span></h3><div role="note" class="hatnote navigation-not-searchable">')
     'controversy in metaphysics on realism versus anti-realism. Implication: Strict or material'
     '''
 
-    text = stripCharacters(text)
-    text = stripTags(text)
-    text = stripBrackets(text)
+    text = strip_characters(text)
+    text = strip_tags(text)
+    text = strip_brackets(text)
     return text
 
 
 
 
 
-def extractTitle(page):
+def extract_title(page):
     '''
     >>> t = '<h1 id="firstHeading" class="firstHeading" lang="en">Logic</h1>            <div id="bodyContent" class="mw-body-content">              <div id="siteSub" class="noprint">From Wikipedia, the free encyclopedia</div>'
-    >>> extractTitle(t)
+    >>> extract_title(t)
     'Logic'
     '''
 
     match = re.search(r'<h1 id="firstHeading" class="firstHeading" lang="en">(.*?)</h1>',page)
     return match.groups()[0]
 
-def extractSummary(page):
+def extract_summary(page):
     '''
     >>> with open('tests.txt','r') as f:
     ...     page = f.read()
-    >>> s = extractSummary(page)
+    >>> s = extract_summary(page)
     >>> s.startswith('Logic (from the Ancient Greek: ')
     True
     >>> s.endswith('studied in computer science, linguistics, psychology, and other fields.')
     True
     '''
 
-    body = getBody(page)
+    body = get_body(page)
     body = body[:re.search(r'<h2>',body).start()]
 
-    body = getParagraphs(body)
+    body = get_paragraphs(body)
 
-    return cleanText(body)
+    return clean_text(body)
 
-def extractText(page):
+def extract_text(page):
     '''
     >>> with open('tests.txt','r') as f:
     ...     page = f.read()
-    >>> body = extractText(page)
+    >>> body = extract_text(page)
     >>> body.startswith('Logic (from the Ancient Greek: λογική, translit. ;logikḗ), originally meaning "the word" or "what is spoken" (but coming to mean "thought" or "reason"), is generally held to consist of the systematic study of the form of valid inference.')
     True
     '''
 
-    body = getBody(page)
-    body = getParagraphs(body)
-    return cleanText(body)
+    body = get_body(page)
+    body = get_paragraphs(body)
+    return clean_text(body)
 
-def extractLinks(page):
+def extract_links(page):
     '''
     >>> sample = ' of <a href="/wiki/Inference" title="Inference">inference</a>, including <a href="/wiki/Fallacies" class="mw-redirect" title="Fallacies">fallacies</a>, and the study of <a href="/wiki/Semantics" title="Semantics">semantics</a>, including <a href="/wiki/Paradox" title="Paradox">paradoxes</a>. Historically, logic has been studied in <a href="/wiki/Philosophy" title="Philosophy">philosophy</a> (since ancient times) and <a href="/wiki/Mathematics" title="Mathematics">mathematics</a> (since the mid-19th century), and recently logic has been studied in <a href="/wiki/Computer_science" title="Computer science">computer science</a>, <a href="/wiki/Linguistics" title="Linguistics">linguistics</a>, <a href="/wiki/Psychology" title="Psychology">psychology</a>, and other fields.</p>'
-    >>> links = extractLinks(sample)
+    >>> links = extract_links(sample)
     >>> len(links)
     9
     >>> links[0] == {'text': 'inference', 'title': 'Inference', 'url': 'http://en.wikipedia.org/wiki/Inference'}
@@ -150,7 +150,7 @@ def extractLinks(page):
             links.append({'url':'http://en.wikipedia.org/wiki/'+a,'title':b,'text':c})
     return links
 
-def splitIntoSentences(text):
+def split_into_sentences(text):
     sentenceDivider = '([.!?][\)]?) (?=[\(]?[A-Z])'
     temp = []
     s = re.split(sentenceDivider,text+' A')
@@ -158,17 +158,13 @@ def splitIntoSentences(text):
         temp.append(s[i] + s[i+1])
     return temp
 
-def splitIntoWords(sentence):
+def split_into_words(sentence):
     s = sentence.split(' ')
     temp = []
     for e in s:
         temp += re.split(r'([,\(\).!?])',e)
     return [e for e in temp if e != '']
 
-def splitText(text):
-    sentences = splitIntoSentences(text)
-    return [splitIntoWords(sentence) for sentence in sentences]
-
-
-
-
+def split_text(text):
+    sentences = split_into_sentences(text)
+    return [split_into_words(sentence) for sentence in sentences]
--- tests.py
+++ tests.py
@@ -2,181 +2,184 @@ import unittest
 import doctest
 import scrape
 
-def setUpModule():
-    global page
-    with open('tests.txt','r') as f:
-        page = f.read()
+with open('tests.txt', 'r') as f:
+    page = f.read()
 
 class TestFetch(unittest.TestCase):
     def test_fetch_page(self):
-        self.assertEqual(scrape.makeUrl('Web_scraping'),'http://en.wikipedia.org/wiki/Web_scraping')
+        self.assertEqual(scrape.make_url('Web_scraping'),
+                         'http://en.wikipedia.org/wiki/Web_scraping')
 
 class TestExtractTitle(unittest.TestCase):
     def test_extract_title(self):
-        global page
-        self.assertEqual(len(page),296351)
+        self.assertEqual(len(page), 296351)
 
-        title = scrape.extractTitle(page)
+        title = scrape.extract_title(page)
 
-        self.assertEqual(title,'Logic')
+        self.assertEqual(title, 'Logic')
 
 class TestExtractLinks(unittest.TestCase):
     def test_extract_links(self):
-        global page
-        self.assertEqual(len(page),296351)
+        self.assertEqual(len(page), 296351)
 
-        links = scrape.extractLinks(page)
+        links = scrape.extract_links(page)
 
-        self.assertEqual(len(links),948)
+        self.assertEqual(len(links), 948)
 
         self.assertEqual(links[10], {'text': 'Ethicists',
                                      'title': 'List of ethicists',
-                                     'url': 'http://en.wikipedia.org/wiki/List_of_ethicists'})
+                                     'url': 'http://en.wikipedia.org/wiki/'
+                                            'List_of_ethicists'})
         self.assertEqual(links[401], {'text': 'monotonicity of entailment',
                                       'title': 'Monotonicity of entailment',
-                                      'url': 'http://en.wikipedia.org/wiki/Monotonicity_of_entailment'})
+                                      'url': 'http://en.wikipedia.org/wiki/'
+                                             'Monotonicity_of_entailment'})
 
 class TestExtractSummary(unittest.TestCase):
     def test_extract_summary(self):
-        global page
-        self.assertEqual(len(page),296351)
+        self.assertEqual(len(page), 296351)
 
-        summary = scrape.extractSummary(page)
-        self.assertTrue(summary.startswith('Logic (from the Ancient Greek: '))
-        self.assertTrue(summary.endswith('studied in computer science, linguistics, psychology, and other fields.'))
+        summary = scrape.extract_summary(page)
+
+        expected_prefix = 'Logic (from the Ancient Greek: '
+        self.assertTrue(summary.startswith(expected_prefix))
+
+
+        expected_suffix = 'studied in computer science, linguistics, ' + \
+                          'psychology, and other fields.'
+        self.assertTrue(summary.endswith(expected_suffix))
 
 class TestExtractBody(unittest.TestCase):
     def test_extract_body(self):
-        global page
-        self.assertEqual(len(page),296351)
+        self.assertEqual(len(page), 296351)
 
-        body = scrape.extractText(page)
+        body = scrape.extract_text(page)
         self.assertTrue(body.startswith('Logic (from the Ancient Greek: '))
-        self.assertTrue(body.endswith('with a view to shocking conventional readers" in his book A History of Western Philosophy.'))
+        self.assertTrue(body.endswith('with a view to shocking conventional '
+            'readers" in his book A History of Western Philosophy.'))
 
 class TestSplitIntoSentences(unittest.TestCase):
     def test_basic_split_sentences(self):
         text = 'A b c. D e f. G h i.'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['A b c.', 'D e f.', 'G h i.'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['A b c.', 'D e f.', 'G h i.'])
 
         text = 'A b c! D e f! G h i!'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['A b c!', 'D e f!', 'G h i!'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['A b c!', 'D e f!', 'G h i!'])
 
         text = 'A b c? D e f? G h i?'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['A b c?', 'D e f?', 'G h i?'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['A b c?', 'D e f?', 'G h i?'])
 
         text = 'A b c. D e f? G h i!'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['A b c.', 'D e f?', 'G h i!'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['A b c.', 'D e f?', 'G h i!'])
 
     def test_split_with_parentheses(self):
         text = 'Abc def. Ghi jkl. Mno pqr.'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['Abc def.', 'Ghi jkl.', 'Mno pqr.'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['Abc def.', 'Ghi jkl.', 'Mno pqr.'])
 
         text = 'Abc def. (Ghi) jkl. Mno pqr.'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['Abc def.', '(Ghi) jkl.', 'Mno pqr.'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['Abc def.', '(Ghi) jkl.', 'Mno pqr.'])
 
         text = 'Abc def. Ghi (jkl). Mno pqr.'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['Abc def.', 'Ghi (jkl).', 'Mno pqr.'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['Abc def.', 'Ghi (jkl).', 'Mno pqr.'])
 
         text = 'Abc def. Ghi (jkl.) Mno pqr.'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['Abc def.', 'Ghi (jkl.)', 'Mno pqr.'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['Abc def.', 'Ghi (jkl.)', 'Mno pqr.'])
 
         text = 'Abc def. Ghi (jkl) mno. Pqr.'
-        s = scrape.splitIntoSentences(text)
-        self.assertEqual(s,['Abc def.', 'Ghi (jkl) mno.', 'Pqr.'])
+        s = scrape.split_into_sentences(text)
+        self.assertEqual(s, ['Abc def.', 'Ghi (jkl) mno.', 'Pqr.'])
 
 class TestSplitIntoWords(unittest.TestCase):
     def test_basic_split_words(self):
         text = 'A b c.'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['A', 'b', 'c', '.'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['A', 'b', 'c', '.'])
 
         text = 'A b c!'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['A', 'b', 'c', '!'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['A', 'b', 'c', '!'])
 
         text = 'A b c?'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['A', 'b', 'c', '?'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['A', 'b', 'c', '?'])
 
         text = 'Abc def ghi.'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['Abc', 'def', 'ghi', '.'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['Abc', 'def', 'ghi', '.'])
 
         text = 'Abc def ghi!'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['Abc', 'def', 'ghi', '!'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['Abc', 'def', 'ghi', '!'])
 
         text = 'Abc def ghi?'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['Abc', 'def', 'ghi', '?'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['Abc', 'def', 'ghi', '?'])
 
     def test_basic_split_with_parentheses(self):
         text = '(A b) c.'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['(', 'A', 'b', ')', 'c', '.'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['(', 'A', 'b', ')', 'c', '.'])
 
         text = 'A (b) c.'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['A', '(', 'b', ')', 'c', '.'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['A', '(', 'b', ')', 'c', '.'])
 
         text = 'A (b c).'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['A', '(', 'b', 'c', ')', '.'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['A', '(', 'b', 'c', ')', '.'])
 
         text = 'A (b c.)'
-        s = scrape.splitIntoWords(text)
-        self.assertEqual(s,['A', '(', 'b', 'c', '.', ')'])
+        s = scrape.split_into_words(text)
+        self.assertEqual(s, ['A', '(', 'b', 'c', '.', ')'])
 
 
 
 class TestSplitText(unittest.TestCase):
     def test_basic_split(self):
         text = 'A b c. D e f. G h i.'
-        s = scrape.splitText(text)
-        self.assertEqual(s,[['A', 'b', 'c', '.'], ['D', 'e', 'f', '.'], ['G', 'h', 'i', '.']])
+        s = scrape.split_text(text)
+        self.assertEqual(s, [['A', 'b', 'c', '.'], ['D', 'e', 'f', '.'], ['G', 'h', 'i', '.']])
 
         text = 'A b c! D e f! G h i!'
-        s = scrape.splitText(text)
-        self.assertEqual(s,[['A', 'b', 'c', '!'], ['D', 'e', 'f', '!'], ['G', 'h', 'i', '!']])
+        s = scrape.split_text(text)
+        self.assertEqual(s, [['A', 'b', 'c', '!'], ['D', 'e', 'f', '!'], ['G', 'h', 'i', '!']])
 
         text = 'A b c? D e f? G h i?'
-        s = scrape.splitText(text)
-        self.assertEqual(s,[['A', 'b', 'c', '?'], ['D', 'e', 'f', '?'], ['G', 'h', 'i', '?']])
+        s = scrape.split_text(text)
+        self.assertEqual(s, [['A', 'b', 'c', '?'], ['D', 'e', 'f', '?'], ['G', 'h', 'i', '?']])
 
         text = 'A b c. D e f! G h i?'
-        s = scrape.splitText(text)
-        self.assertEqual(s,[['A', 'b', 'c', '.'], ['D', 'e', 'f', '!'], ['G', 'h', 'i', '?']])
+        s = scrape.split_text(text)
+        self.assertEqual(s, [['A', 'b', 'c', '.'], ['D', 'e', 'f', '!'], ['G', 'h', 'i', '?']])
 
     def test_split_with_parentheses(self):
         text = 'A b c. (D e) f. G h i.'
-        s = scrape.splitText(text)
-        self.assertEqual(s,[['A', 'b', 'c', '.'],
-                            ['(', 'D', 'e', ')', 'f', '.'],
-                            ['G', 'h', 'i', '.']])
+        s = scrape.split_text(text)
+        self.assertEqual(s, [['A', 'b', 'c', '.'],
+                             ['(', 'D', 'e', ')', 'f', '.'],
+                             ['G', 'h', 'i', '.']])
 
         text = 'A b c. D (e f.) G h i.'
-        s = scrape.splitText(text)
-        self.assertEqual(s,[['A', 'b', 'c', '.'],
-                            ['D', '(', 'e', 'f', '.', ')'],
-                            ['G', 'h', 'i', '.']])
+        s = scrape.split_text(text)
+        self.assertEqual(s, [['A', 'b', 'c', '.'],
+                             ['D', '(', 'e', 'f', '.', ')'],
+                             ['G', 'h', 'i', '.']])
 
     def test_split_real_text(self):
-        global page
-        self.assertEqual(len(page),296351)
-        text = scrape.extractText(page)
+        self.assertEqual(len(page), 296351)
+        text = scrape.extract_text(page)
 
-        spl = scrape.splitText(text)
-        self.assertEqual(spl[13],['It', 'is', 'necessary', 'because', 'indicative', 'sentences', 'of', 'ordinary', 'language', 'show', 'a', 'considerable', 'variety', 'of', 'form', 'and', 'complexity', 'that', 'makes', 'their', 'use', 'in', 'inference', 'impractical', '.'])
-        self.assertEqual(spl[21],['The', 'concrete', 'terms', '"man"', ',', '"mortal"', ',', 'etc', '.', ',', 'are', 'analogous', 'to', 'the', 'substitution', 'values', 'of', 'the', 'schematic', 'placeholders', 'P', ',', 'Q', ',', 'R', ',', 'which', 'were', 'called', 'the', '"matter"', '(', 'Greek', 'hyle', ')', 'of', 'the', 'inference', '.'])
+        spl = scrape.split_text(text)
+        self.assertEqual(spl[13], ['It', 'is', 'necessary', 'because', 'indicative', 'sentences', 'of', 'ordinary', 'language', 'show', 'a', 'considerable', 'variety', 'of', 'form', 'and', 'complexity', 'that', 'makes', 'their', 'use', 'in', 'inference', 'impractical', '.'])
+        self.assertEqual(spl[21], ['The', 'concrete', 'terms', '"man"', ',', '"mortal"', ',', 'etc', '.', ',', 'are', 'analogous', 'to', 'the', 'substitution', 'values', 'of', 'the', 'schematic', 'placeholders', 'P', ',', 'Q', ',', 'R', ',', 'which', 'were', 'called', 'the', '"matter"', '(', 'Greek', 'hyle', ')', 'of', 'the', 'inference', '.'])
 
 
 
